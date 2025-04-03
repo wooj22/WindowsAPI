de1e8a7aa61b05cd18eb1ed6fea938521580bff2
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-04-03 19:20:53 +0900

    코드정리

---
+++ WindowsAPI_GDIplus/AnimationDraw.cpp
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <windows.h>
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+
+
+LPCTSTR g_szClassName = TEXT("윈도우 클래스 이름");
+
+// Window Size
+int g_width = 1024;
+int g_height = 768;
+
+// HDC (Device Context) 
+HWND g_hWnd;
+HDC g_FrontBufferDC;
+HDC g_BackBufferDC;
+
+// BITMAP
+HBITMAP g_BackBufferBitmap;
+
+
+/*----------------------- Funtions -------------------------*/
+/// 콘솔창 생성
+void InitConsole()
+{
+	AllocConsole();
+	FILE* fp;
+	freopen_s(&fp, "CONOUT$", "w", stdout);
+	SetConsoleTitle(L"윈도우 메시지 콘솔 로그");
+	printf("콘솔 로그 시작...\n\n");
+}
+
+/// 콘솔창 해제
+void UninitConsole()
+{
+	fclose(stdout);
+	FreeConsole();
+}
+
+/// 마지막으로 발생한 WIN32 API 에러의 메시지를 출력
+void PrintLastErrorMessage()
+{
+	// 최근 발생한 오류 코드
+	DWORD errorCode = GetLastError();
+	LPVOID lpMsgBuf;
+
+	// 오류 코드에 해당하는 시스템 메시지
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		errorCode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPSTR)&lpMsgBuf,
+		0,
+		NULL);
+
+	if (lpMsgBuf)
+	{
+		printf("오류 코드: %lu\n오류 메시지: %s\n", errorCode, (char*)lpMsgBuf);
+		LocalFree(lpMsgBuf); // FormatMessageA에서 할당된 버퍼 해제
+	}
+	else
+	{
+		printf("오류 코드: %lu (메시지를 찾을 수 없음)\n", errorCode);
+	}
+}
+
+
+/// 윈도우 프로시저 메시지 콜백
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	// 기본 창 프로시저로 메시지 전달
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+
+/*-------------------------- Main ----------------------------*/
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// 콘솔창 생성
+	InitConsole();
+
+	// 디렉토리 경로 저장
+	char szPath[MAX_PATH] = { 0, };
+	::GetCurrentDirectoryA(MAX_PATH, szPath);
+
+	// WNDCLASS 정의
+	WNDCLASS wc = { 0 };
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = g_szClassName;
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+
+	// RegisterClass : WNDCLASS 등록
+	RegisterClass(&wc);
+
+	// 창 크기 조절
+	RECT rcClient = { 0, 0, (LONG)g_width, (LONG)g_height };
+	AdjustWindowRect(&rcClient, WS_OVERLAPPEDWINDOW, FALSE);
+
+	// 윈도우 생성
+	HWND hwnd = CreateWindow(
+		g_szClassName,
+		TEXT("GDL plus 애니메이션 과제중"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		rcClient.right - rcClient.left, rcClient.bottom - rcClient.top,
+		NULL, NULL, hInstance, NULL);
+
+
+	// Show
+	ShowWindow(hwnd, nCmdShow);
+
+	// WM_PAINT 이벤트 생성
+	UpdateWindow(hwnd);
+
+	// 더블 버퍼링 초기화
+	g_FrontBufferDC = GetDC(hwnd);
+	g_BackBufferDC = CreateCompatibleDC(g_FrontBufferDC);
+	g_BackBufferBitmap = CreateCompatibleBitmap(g_FrontBufferDC, g_width, g_height);
+	SelectObject(g_BackBufferDC, g_BackBufferBitmap);
+
+	// GDI+ 초기화
+	ULONG_PTR g_GdiPlusToken;
+	Gdiplus::GdiplusStartupInput gsi;
+	Gdiplus::GdiplusStartup(&g_GdiPlusToken, &gsi, nullptr);
+
+	// g_BackBufferDC와 호환되는 그래픽 객체 생성
+	Gdiplus::Graphics* g_pBackBufferGraphics = Gdiplus::Graphics::FromHDC(g_BackBufferDC);
+
+	// 이미지 로드
+	Gdiplus::Bitmap* g_pImageBitmap = new Gdiplus::Bitmap(L"../Resource/PlayerAnimation/attack-A1.png");
+	UINT witdh = g_pImageBitmap->GetWidth();
+	UINT height = g_pImageBitmap->GetHeight();
+
+	if (g_pImageBitmap->GetLastStatus() != Gdiplus::Ok)
+	{
+		MessageBox(hwnd, L"PNG 파일 로드 실패", L"오류", MB_ICONERROR);
+		PostQuitMessage(0);
+	}
+
+	// Render
+	MSG msg;
+	while (true)
+	{
+		// 메시지 get
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				break;
+
+			TranslateMessage(&msg);	// 키보드 입력 메시지
+			DispatchMessage(&msg);	// 메시지를 윈도우 프로시저로 전달
+		}
+
+		PatBlt(g_BackBufferDC, 0, 0, g_width, g_height, BLACKNESS);
+
+		g_pBackBufferGraphics->DrawImage(g_pImageBitmap,
+			g_width / 2 - (int)g_pImageBitmap->GetWidth() / 2, g_height / 2 - (int)g_pImageBitmap->GetHeight() / 2);
+
+		BitBlt(g_FrontBufferDC, 0, 0, g_width, g_height, g_BackBufferDC, 0, 0, SRCCOPY);
+	}
+
+	// GDI+ 해제
+	delete g_pImageBitmap;
+	delete g_pBackBufferGraphics;
+	Gdiplus::GdiplusShutdown(g_GdiPlusToken);
+
+	DeleteObject(g_BackBufferBitmap);
+	DeleteDC(g_BackBufferDC);
+	ReleaseDC(hwnd, g_FrontBufferDC);
+
+	// 콘솔 출력 해제
+	UninitConsole();  
+	return (int)msg.wParam;
+}
